8c6e3eee492bf22e0f66610956eb2f5323903044
Oliver1334 <oliver.taylor95@outlook.com>
2026-08-31 00:30:45 +0100

    Boss stats class added

---
+++ MorndulCryptsConsole/BossStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorndulCryptsConsole
+{
+    internal class BossStats
+    {
+
+        //Fields
+        public int health;
+        public int maxHealth;
+        public int attack;
+        public int gold;
+        public int xp;
+
+
+        //Constructor
+
+        public BossStats(int health, int maxHealth, int attack, int gold, int xp)
+        {
+            this.health = health;
+            this.maxHealth = maxHealth;
+            this.attack = attack;
+            this.gold = gold;
+            this.xp = xp;
+        }
+    }
+}
